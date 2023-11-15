b8818bd66f09a78e14962e93c41dea6b900b28b5
Paul Sepulveda <paulgsepulveda@gmail.com>
2023-11-15 16:05:17 -0500

    initial commit, updated boilerplate

---
+++ template-pack.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: CECS Template Pack
+Description: A set of WordPress page templates with associated ACF files
+Version: 1.0.0
+Author: Paul Sepulveda
+License: GPL3
+GitHub Plugin URI: UCF-CECS-IT/CECS-Template-Pack
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
